8ca5032c3c8886769cfe80497fbcf9003cdec048
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-08 21:10:07 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -39,7 +39,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 "SELECT users.name, pledges.amount
  FROM users
  JOIN pledges
- ON users.id = pledges.users_id
+ ON users.id = pledges.user_id
  GROUP BY users.name
  ORDER BY pledges.amount, users.name;"
 end
